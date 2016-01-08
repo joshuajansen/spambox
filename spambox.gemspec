827e3e174da6ad8bc82576cda19c421ebd8dfe63
Joshua Jansen <joshuajansen88@gmail.com>
2016-01-08 10:22:24 +0100

    Squish string to strip all whitespace characters
    - Adds Rubocop with configuration
    - Refactor and test hash flattening method
    - Use proper name for active support gem
    - Bump version

--- spambox.gemspec
+++ spambox.gemspec
@@ -1,15 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 Gem::Specification.new do |gem|
-  gem.name          = 'spambox'
-  gem.version       = '0.2'
-  gem.date          = '2015-12-04'
+  gem.name          = "spambox"
+  gem.version       = "0.3.2"
+  gem.date          = "2016-01-08"
   gem.authors       = ["Joshua Jansen"]
   gem.email         = ["joshuajansen88@gmail.com"]
-  gem.description   = %q{Spambox uses Formbox.es API to check for spam.}
-  gem.summary       = %q{Spambox is a gem that checks your objects for unsafe keywords and returns a spam score.}
+  gem.description   = "Spambox uses Formbox.es API to check for spam."
+  gem.summary       = "Spambox is a gem that checks your objects
+    for unsafe keywords and returns a spam score."
   gem.homepage      = "https://github.com/joshuajansen/spambox"
   gem.files         = ["lib/spambox.rb"]
-  gem.license       = 'MIT'
+  gem.license       = "MIT"
   gem.add_runtime_dependency "sanitize", "~> 4.0"
 end
--- test/test_spam_filter.rb
+++ test/test_spam_filter.rb
@@ -1,6 +1,6 @@
-require 'minitest/autorun'
-require 'spambox'
-require 'support/entry_mock'
+require "minitest/autorun"
+require "spambox"
+require "support/entry_mock"
 
 class TestSpambox < Minitest::Test
   def test_only_spam_words_is_100
@@ -12,11 +12,11 @@ class TestSpambox < Minitest::Test
   end
 
   def test_safe_string_with_html_allowed_is_0
-    assert_equal Spambox.new("<a href='#foo'>", { allow_html: true }).spam_score, 0
+    assert_equal Spambox.new("<a href='#foo'>", allow_html: true).spam_score, 0
   end
 
   def test_unsafe_string_with_html_allowed_is_100
-    assert_equal Spambox.new("<a href='#foo'>pharmacy</a>", { allow_html: true }).spam_score, 100
+    assert_equal Spambox.new("<a href='#foo'>pharmacy</a>", allow_html: true).spam_score, 100
   end
 
   def test_half_spam_words_is_50
@@ -30,5 +30,32 @@ class TestSpambox < Minitest::Test
   def test_active_record_object_for_spam
     object = EntryMock.new("Billion dollar Pharmacy", "Cheap banana pancakes")
     assert_equal Spambox.new(object).spam_score, 50
+  end
+
+  def test_line_breaks_should_pass_spambox
+    object = EntryMock.new(
+      "joshua@firmhouse.com",
+      "Hello, \r\n\r\nI like to get in contact!\r\n
+      Looking forward to hearing from you. \r\nBye, \r\n John"
+    )
+
+    assert_equal Spambox.new(object).spam_score, 0
+  end
+
+  def test_flatten_hash_values_returns_values_from_nested_hash
+    hash = {
+      form: {
+        name: "John",
+        email: "john@example.com",
+        body: "Hi!",
+        address: {
+          street: "Mainstreet 1"
+        }
+      }
+    }
+
+    flat_hash = Spambox.new(hash).send(:flatten_hash_values, hash)
+
+    assert_equal flat_hash, ["John", "john@example.com", "Hi!", "Mainstreet 1"]
   end
 end
